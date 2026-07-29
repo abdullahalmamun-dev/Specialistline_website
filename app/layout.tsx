0c4ca47da70d8e670a10a9e95bab76636d21f734
abdullahalmamun-dev <mdabdullah.allinfozone@gmail.com>
2026-07-29 20:08:45 +0600

    Update 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { headers } from "next/headers";
-import "./globals.css";
+import "./globals.css"
 
 export async function generateMetadata(): Promise<Metadata> {
   const requestHeaders = await headers();
