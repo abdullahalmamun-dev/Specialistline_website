439579067f1de1a9c22b4bcab9272ec10e725729
abdullahalmamun-dev <mdabdullah.allinfozone@gmail.com>
2026-07-30 14:06:10 +0600

    Updated message format

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -17,23 +17,23 @@ export default function Terms() {
       color: 'var(--dark)'
     }}>
       <p style={{ marginBottom: '15px' }}>
-        <strong>Peachtree Capital Group LLC</strong><br/>
-        Updates regarding your financial consulting and capital programs.
+        [Business name]<br/>
+        [Insert Business description here; this is simply a brief description of the kinds of messages users can expect to receive when they opt in.]
       </p>
       <p style={{ marginBottom: '15px' }}>
-        You can cancel the SMS service at any time. Just text "STOP" to our number. After you send the SMS message "STOP" to us, we will send you an SMS message to confirm that you have been unsubscribed. After this, you will no longer receive SMS messages from us. If you want to join again, just sign up as you did the first time and we will start sending SMS messages to you again.
+        You can cancel the SMS service at any time. Just text "STOP" to the [Phone Number]. After you send the SMS message "STOP" to us, we will send you an SMS message to confirm that you have been unsubscribed. After this, you will no longer receive SMS messages from us. If you want to join again, just sign up as you did the first time and we will start sending SMS messages to you again.
       </p>
       <p style={{ marginBottom: '15px' }}>
-        If you are experiencing issues with the messaging program you can reply with the keyword HELP for more assistance, or you can get help directly at <a href="mailto:info@peachtree.capital" style={{ textDecoration: 'underline', color: 'var(--peach)' }}>info@peachtree.capital</a>.
+        If you are experiencing issues with the messaging program you can reply with the keyword HELP for more assistance, or you can get help directly at [support email address or toll-free number].
       </p>
       <p style={{ marginBottom: '15px' }}>
-        Carriers are not liable for delayed or undelivered messages.
+        Carriers are not liable for delayed or undelivered messages
       </p>
       <p style={{ marginBottom: '15px' }}>
-        As always, message and data rates may apply for any messages sent to you from us and to us from you. You will receive recurring messages. If you have any questions about your text plan or data plan, it is best to contact your wireless provider.
+        As always, message and data rates may apply for any messages sent to you from us and to us from you. You will receive [message frequency]. If you have any questions about your text plan or data plan, it is best to contact your wireless provider.
       </p>
       <p style={{ marginBottom: '0' }}>
-        If you have any questions regarding privacy, please read our privacy policy: <a href="/privacy" style={{ textDecoration: 'underline', color: 'var(--peach)' }}>Privacy Policy</a>.
+        If you have any questions regarding privacy, please read our privacy policy: [link to privacy policy]
       </p>
     </blockquote>
 
